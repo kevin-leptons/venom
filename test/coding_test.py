ebb5dd195aeac9bd4712a3682f1d7f02753b3674
kevin.leptons@gmail.com <kevin.leptons@gmail.com>
2016-12-01 11:21:36 +0700

    fix test

--- test/coding_test.py
+++ test/coding_test.py
@@ -3,11 +3,9 @@ from subprocess import Popen
 
 
 root = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-exclude_files = 'venv,build,dest,dist,tmp,__init__.py,.cache,.*,conf.py'
+exclude_files = 'venv,build,dest,dist,tmp,__init__.py,.*,conf.py'
 
 
 def test_coding():
     cmd = ['flake8', root, '--exclude', exclude_files]
-    res = Popen(cmd).wait()
-
-    assert res == 0
+    assert Popen(cmd).wait() == 0
